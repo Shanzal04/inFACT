1a19a9e5f80b9aa49475f82abcbbf0d6ef32b760
Shanzal04 <114459742+Shanzal04@users.noreply.github.com>
2023-10-03 02:18:29 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost/newsletterDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create a schema for subscribers
+const subscriberSchema = new mongoose.Schema({
+  email: String,
+});
+
+// Create a model based on the schema
+const Subscriber = mongoose.model('Subscriber', subscriberSchema);
+
+// Serve static files (e.g., HTML and CSS)
+app.use(express.static('public'));
+
+// Define a route to handle the form submission
+app.post('/subscribe', (req, res) => {
+  const email = req.body.email;
+
+  // Create a new subscriber document
+  const newSubscriber = new Subscriber({
+    email,
+  });
+
+  // Save the new subscriber to
